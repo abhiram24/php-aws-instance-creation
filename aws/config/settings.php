e3983c52f06484c5807640c82602528fc0bd7054
abhiram24 <abhiram.sampath@gmail.com>
2017-02-26 13:32:23 +0400

    Update settings.php

--- aws/config/settings.php
+++ aws/config/settings.php
@@ -2,8 +2,8 @@
 
 $key = '<<YOUR_KEY_HERE>>';
 $secret = '<<YOUR_SECRET_PASSWORD_HERE>>';
-$keyPairName = 'suitepad3-keypair';
-$securityGroupName ='suitepad3-test-group';
+$keyPairName = 'sp3-keypair';
+$securityGroupName ='sp3-test-group';
 $ImageId = 'ami-7172b611';
 $InstanceType = 't2.micro';
 
